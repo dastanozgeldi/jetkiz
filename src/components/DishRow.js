5468c997c0ef5d074e0a33326b1f4c915d8c9a52
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-20 19:44:53 +0600

    feat(*): redux init & basket management setup

--- src/components/DishRow.js
+++ src/components/DishRow.js
@@ -3,9 +3,26 @@ import React, { useState } from "react";
 import Currency from "react-currency-formatter";
 import { urlFor } from "../sanity";
 import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from "../slices/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+    dispatch(removeFromBasket({ id }));
+  };
 
   return (
     <>
@@ -36,17 +53,19 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center gap-2 pb-3">
-            <TouchableOpacity>
+            <TouchableOpacity
+              disabled={!items.length}
+              onPress={removeItemFromBasket}
+            >
               <MinusCircleIcon
-                color="#00CCBB"
-                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                color={items.length > 0 ? "#00CCBB" : "gray"}
                 size={40}
               />
             </TouchableOpacity>
 
-            <Text>0</Text>
+            <Text>{items.length}</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={addItemToBasket}>
               <PlusCircleIcon color="#00CCBB" size={40} />
             </TouchableOpacity>
           </View>
--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -10,6 +10,7 @@ import {
   StarIcon,
 } from "react-native-heroicons/solid";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -35,67 +36,70 @@ const RestaurantScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <View className="relative">
-        <Image
-          source={{ uri: urlFor(imgUrl).url() }}
-          className="w-full h-64 bg-gray-300 p-4"
-        />
-        <TouchableOpacity
-          onPress={navigation.goBack}
-          className="absolute top-14 left-5 bg-gray-100 rounded-full p-2"
-        >
-          <ArrowLeftIcon size={20} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
+    <>
+      <BasketIcon />
+      <ScrollView>
+        <View className="relative">
+          <Image
+            source={{ uri: urlFor(imgUrl).url() }}
+            className="w-full h-64 bg-gray-300 p-4"
+          />
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="absolute top-14 left-5 bg-gray-100 rounded-full p-2"
+          >
+            <ArrowLeftIcon size={20} color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
 
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row gap-2 my-1">
-            <View className="flex-row items-center space-x-">
-              <StarIcon color="green" opacity={0.5} size={22} />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-600">{rating}</Text> - {genre}
-              </Text>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row gap-2 my-1">
+              <View className="flex-row items-center space-x-">
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-600">{rating}</Text> - {genre}
+                </Text>
+              </View>
+
+              <View className="flex-row items-center space-x-">
+                <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-600">Nearby</Text> - {address}
+                </Text>
+              </View>
             </View>
 
-            <View className="flex-row items-center space-x-">
-              <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-600">Nearby</Text> - {address}
-              </Text>
-            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
           </View>
 
-          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          <TouchableOpacity className="flex-row items-center gap-2 p-4 border-y border-gray-300">
+            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={22} />
+            <Text className="pl-2 flex-1 text-md font-bold">
+              Have a food allergy?
+            </Text>
+            <ChevronRightIcon color="#00CCBB" />
+          </TouchableOpacity>
+
+          <View className="pb-32">
+            <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+            {/* Dishrows */}
+            {dishes.map((dish) => (
+              <DishRow
+                key={dish._id}
+                id={dish._id}
+                name={dish.name}
+                description={dish.short_description}
+                price={dish.price}
+                image={dish.image}
+              />
+            ))}
+          </View>
         </View>
-
-        <TouchableOpacity className="flex-row items-center gap-2 p-4 border-y border-gray-300">
-          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={22} />
-          <Text className="pl-2 flex-1 text-md font-bold">
-            Have a food allergy?
-          </Text>
-          <ChevronRightIcon color="#00CCBB" />
-        </TouchableOpacity>
-
-        <View>
-          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-
-          {/* Dishrows */}
-          {dishes.map((dish) => (
-            <DishRow
-              key={dish._id}
-              id={dish._id}
-              name={dish.name}
-              description={dish.short_description}
-              price={dish.price}
-              image={dish.image}
-            />
-          ))}
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
