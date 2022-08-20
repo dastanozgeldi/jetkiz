f45f4ea8838bf37abb0145236c2aa41775959b1f
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-20 15:40:45 +0600

    feat(*): sanity init & design homepage

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { StarIcon } from "react-native-heroicons/solid";
+import { LocationMarkerIcon } from "react-native-heroicons/outline";
+import { urlFor } from "../sanity";
+
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_description,
+  dishes,
+  long,
+  lat,
+}) => {
+  return (
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        source={{ uri: urlFor(imgUrl).url() }}
+        className="h-36 w-64 rounded"
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-xl pt-2">{title}</Text>
+        <View className="flex-row items-center gap-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-600">{rating}</Text> - {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center gap-2">
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby - {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default RestaurantCard;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  AdjustmentsIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className="mt-5 text-center text-red-500">Hello World!</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+    *[_type == "featured"] {
+      ...,
+      restaurants[] ->{
+        ...,
+        dishes[]->,
+        type->{
+          name
+        }
+      },
+    }
+    `
+      )
+      .then((data) => {
+        setFeaturedCategories(data);
+      });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row p-3 items-center mx-4 gap-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Almaty <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center gap-2 pb-2 mx-2">
+        <View className="flex-row items-center gap-2 flex-1 bg-gray-200 p-3 rounded-xl">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView>
+        {/* Categories (carousel) */}
+        <Categories />
+
+        {/* Featured Rows */}
+        {featuredCategories?.map((category) => (
+          <FeaturedRow
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
