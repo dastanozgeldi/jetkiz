59ccdcfcb35981f38105233f8d1c2dd7fe6aaf6d
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-02-19 00:59:02 +0600

    feat(schema): add name, description to category

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -10,9 +10,21 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
+      name: "short_description",
+      type: "string",
+      title: "Short Description",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
       name: "image",
       title: "Category Image",
       type: "image",
+    },
+    {
+      name: "dishes",
+      title: "Dishes",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "dish" }] }],
     },
   ],
 };
